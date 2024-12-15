c7a14752d74403a5a2f90f26850ce275a5be497e
cherry-cloud <cherry1615405@gmail.com>
2024-12-15 11:28:56 +0800

    Create count_file.c

---
+++ chapter_01_file/count_file.c
@@ -0,0 +1,46 @@
+#include <sys/types.h>
+#include <dirent.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int getFileNum(const char * path);
+
+int main(int argc, char * argv[]) 
+{
+    if (argc < 2) {
+        printf("%s path\n", argv[0]);
+        return -1;
+    }
+    int num = getFileNum(argv[1]);
+    printf("%d\n", num);
+    return 0;
+}
+
+int getFileNum(const char * path) 
+{
+    DIR * dir = opendir(path);
+    if (dir == NULL) {
+        perror("opendir");
+        exit(0);
+    }
+    struct dirent *ptr;
+    int total = 0;
+    while ((ptr = readdir(dir)) != NULL) {
+        char * dname = ptr->d_name;
+        if (strcmp(dname, ".") == 0 || strcmp(dname, "..") == 0) {
+            continue;
+        }
+        if (ptr->d_type == DT_DIR) {
+            char newpath[256];
+            sprintf(newpath, "%s/%s", path, dname);
+            total += getFileNum(newpath);
+        }
+
+        if (ptr->d_type == DT_REG) {
+            total++;
+        }
+    }
+    closedir(dir);
+    return total;
+}
