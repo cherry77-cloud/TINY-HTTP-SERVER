f23f13179a320f831880b43d6072f40cdcd29a0a
cherry-cloud <cherry1615405@gmail.com>
2024-12-16 21:52:01 +0800

    Create pthread_create.c

---
+++ chapter_03/thread/pthread/pthread_create.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <string.h>
+#include <unistd.h>
+
+void * callback(void * arg);
+
+int main() 
+{
+    pthread_t tid;
+    int num = 10;
+    int ret = pthread_create(&tid, NULL, callback, (void *)&num);
+    if (ret != 0) {
+        char * errstr = strerror(ret);
+        printf("error : %s\n", errstr);
+    }
+    for (int i = 0; i < 5; i++) {
+        printf("%d\n", i);
+    }
+    sleep(1);
+    return 0;
+}
+
+void * callback(void * arg) 
+{
+    printf("child thread...\n");
+    printf("arg value: %d\n", *(int *)arg);
+    return NULL;
+}
